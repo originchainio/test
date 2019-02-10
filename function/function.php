8b59a95800c6fb9a62d66227811dc44540b83948
originchainio <46016927+originchainio@users.noreply.github.com>
2019-02-11 03:29:34 +0800

    Add files via upload

--- function/function.php
+++ function/function.php
@@ -18,14 +18,9 @@ function san_num_ip($a){
     $a = preg_replace("/[^0-9\.]/", "", $a);
     return $a;
 }
-// 检查地址的有效性。目前，长度为>=70和<=128
 function valid_len($str,$minlen=70,$maxlen=128){
-    if ($minlen==='') {
-        $minlen=70;
-    }
-    if ($maxlen==='') {
-        $maxlen=128;
-    }
+    if ($minlen==='') {	$minlen=70;	}
+    if ($maxlen==='') {	$maxlen=128;	}
     if (strlen($str) < $minlen || strlen($str) > $maxlen) {
         return false;
     }else{
@@ -33,7 +28,6 @@ function valid_len($str,$minlen=70,$maxlen=128){
     }
 }
 // check the validity of a base58 encoded key. At the moment, it checks only the characters to be base58.
-// 检查base58编码密钥的有效性。目前，它只检查字符是否为base58
 function valid_base58($str){
     $chars = str_split("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
     for ($i = 0; $i < strlen($str);
@@ -44,7 +38,4 @@ function valid_base58($str){
     }
     return true;
 }
-
-
-
 ?>
