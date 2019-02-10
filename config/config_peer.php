bb7ec3f827478a572ce9da647d856a0954c0e330
originchainio <46016927+originchainio@users.noreply.github.com>
2019-02-10 23:28:50 +0800

    Update config_peer.php

--- config/config_peer.php
+++ config/config_peer.php
@@ -12,13 +12,11 @@ return array(
 	'db_max_peers'=>300,
 
 	// How many new peers to check from each peer
-	'max_test_peers'=>5,	//s随机检测peer 一次最大值
+	'max_test_peers'=>5,	//checke peer max number
 
 	// The initial peers to sync from in sanity
 	'initial_peer_list'=>array(
-							'http://192.168.1.36',
-							'http://192.168.1.37',
-							'http://192.168.1.38',
+							'http://t1.originchain.io',
 							),
 
 	// Bad peer is not add database
